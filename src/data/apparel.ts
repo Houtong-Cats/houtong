ce8ecfb34f5054c35b29cf0aee44b7c9b33b7ad1
James <contact@jamesyap.org>
2024-05-18 21:31:48 -0400

    Added more assets

---
+++ src/data/apparel.ts
@@ -0,0 +1,27 @@
+import shirt1 from "../assets/tops/shirt1.glb";
+import shirt2 from "../assets/tops/shirt2.glb";
+import shirt3 from "../assets/tops/shirt3.glb";
+import shirt4 from "../assets/tops/shirt4.glb";
+import shirt5 from "../assets/tops/shirt5.glb";
+import shirt6 from "../assets/tops/shirt6.glb";
+
+export const tops = [
+  {
+    glbFile: shirt1,
+  },
+  {
+    glbFile: shirt2,
+  },
+  {
+    glbFile: shirt3,
+  },
+  {
+    glbFile: shirt4,
+  },
+  {
+    glbFile: shirt5,
+  },
+  {
+    glbFile: shirt6,
+  },
+];
--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -3,17 +3,15 @@ import Wardrobe from "../assets/wardrobe";
 import Camera from "../components/Camera";
 
 export default function Item() {
-    return (
-        <div className="flex flex-col w-full h-screen">
-            <Navbar /> 
-            <div className="flex flex-row h-screen">
-                <div className="w-1/2 h-fill bg-white">
-                    <Wardrobe />
-                </div>
-                <div className="w-1/2 h-fill bg-black">
-                    <Camera />
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <Navbar />
+      <div className="flex flex-row h-screen">
+        <div className="w-1/2 h-fill bg-white">
+          <Wardrobe />
         </div>
-    );
+        <div className="w-1/2 h-fill bg-black">{/* <Camera /> */}</div>
+      </div>
+    </div>
+  );
 }
